c322d2354230bfcd3c857541eccf3852b8fbdeda
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-01-01 22:30:01 +0100

    Ready for vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,10 +39,7 @@ export default {
 
     // Modules (https://go.nuxtjs.dev/config-modules)
     modules: [
-        // https://go.nuxtjs.dev/axios
         '@nuxtjs/axios',
-        // https://go.nuxtjs.dev/content
-       // '@nuxt/content',
       	'@nuxtjs/sitemap',
     	'@nuxtjs/robots',
     ],

e4022efc2d0ba8a4afb0675da5b53e74e9fb6b35
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-01-01 17:54:46 +0100

    Add navigation, footer & some files

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
-		Hello
+		<section class="h-screen">
+			<div class="max-w-5xl mx-auto px-4 sm:px-6"></div>
+		</section>
+		<section class="h-screen">
+			<div class="max-w-5xl mx-auto px-4 sm:px-6"></div>
+		</section>
+		<section class="h-screen bg-gray-50 border-t border-b border-gray-200">
+			<div class="max-w-5xl mx-auto px-4 sm:px-6"></div>
+		</section>
+		<section class="h-screen">
+			<div class="max-w-5xl mx-auto px-4 sm:px-6"></div>
+		</section>
 	</div>
 </template>
 

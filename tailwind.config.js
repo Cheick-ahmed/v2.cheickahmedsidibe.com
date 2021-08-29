63cf619403c048175ce8d0dec10887c1977f2de6
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2021-08-29 15:35:33 +0200

    update tailwindcss to the latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	purge: [ './components/**/*.{vue,js}', './layouts/**/*.vue', './pages/**/*.vue'],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: 'media', // or 'media' or 'class'
 	theme: {
 		extend: {
 			fontFamily: {
